53a2b87df46152c8feeaa74b4761b447653287ab
Caseres Marina <marinacaseres1997@gmail.com>
2024-02-26 19:07:59 -0300

    Arregladas rutas que no tomaba gh-pages

--- src/components/Inicio/Main/index.jsx
+++ src/components/Inicio/Main/index.jsx
@@ -16,8 +16,8 @@ const Main = ({login, openLogin}) => {
 
     if(login){
         return(
-            <section name="login" className='form-background'>
-                <img className="background-pizza z-2" src='/src/assets/pizza.png'></img>
+            <section name="login" className='form-background'>{/*/src/assets/pizza.png */}
+                <img className="background-pizza z-2" src='./pizza.png '></img>
                 <svg className="background-shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1277.08 767.46">
                     <path d="m317.33,3.41c-34.36,7.41-92.12,24.57-149.75,68.07-84.24,63.58-117.27,144.98-134.43,188.89C20.19,293.52-4.83,359.61.82,447.55c4.48,69.83,26.17,120.31,39.14,149.75,34.65,78.69,83.56,134.31,122.52,170.17h1114.6V0c-319.92,1.14-639.83,2.27-959.75,3.41Z" fill="currentColor"/>
                 </svg>
